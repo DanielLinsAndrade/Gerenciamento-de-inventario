929900e84f04c651a2d18d9cd6487dcb28567714
Aranat404 <daniel.loading404@gmail.com>
2024-12-12 15:08:30 -0300

    test: Adicionando arquivos de teste

--- inventory/models.py
+++ inventory/models.py
@@ -40,7 +40,7 @@ class Item(models.Model):
     )
     quantity = models.IntegerField(default=0)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    description = models.TextField(blank=True, null=True)
+    description = models.CharField(max_length=100)
 
     def __str__(self):
         """
