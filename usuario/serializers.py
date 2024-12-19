d99876734c889b6c4fad780895ab8635f29745b1
DanielLinsAndrade <75346815+DanielLinsAndrade@users.noreply.github.com>
2024-12-19 04:30:15 -0300

    cleanup: removendo comentários automáticos

--- usuario/serializers.py
+++ usuario/serializers.py
@@ -1,7 +1,4 @@
 from rest_framework import serializers
-<<<<<<< HEAD
-from .models import Funcionario
-=======
 from .models import Funcionario, UserProfileExample
 from django.contrib.auth.models import User
 
@@ -10,14 +7,11 @@ class UserProfileExampleSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserProfileExample
         fields = ['id', 'address', 'phone_number', 'birth_date', 'user']
->>>>>>> ce1a5a5 (test: Adicionando testes aos itens e usuários)
 
 class FuncionarioSerializer(serializers.ModelSerializer):
     class Meta:
         model = Funcionario
         fields = '__all__'
-<<<<<<< HEAD
-=======
         
 class FuncionarioCreateSerializer(serializers.Serializer):
     nome = serializers.CharField(max_length=140)
@@ -26,4 +20,3 @@ class FuncionarioCreateSerializer(serializers.Serializer):
     user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
     login = serializers.CharField(max_length=150)
     senha = serializers.CharField(write_only=True)
->>>>>>> ce1a5a5 (test: Adicionando testes aos itens e usuários)
