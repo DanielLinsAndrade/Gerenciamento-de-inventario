e23d955dfcc058ee4e7a20daec018eec0c867ee1
DanielLinsAndrade <75346815+DanielLinsAndrade@users.noreply.github.com>
2024-12-19 04:23:48 -0300

    cleanup: removendo comentários automáticos

--- inventory/serializers.py
+++ inventory/serializers.py
@@ -1,9 +1,7 @@
 from rest_framework import serializers
-<<<<<<< HEAD
-from .models import Category, Item
-=======
+
 from .models import Category, Funcionario
->>>>>>> ce1a5a5 (test: Adicionando testes aos itens e usuários)
+
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
@@ -12,9 +10,5 @@ class CategorySerializer(serializers.ModelSerializer):
 
 class ItemSerializer(serializers.ModelSerializer):
     class Meta:
-<<<<<<< HEAD
-        model = Item
-=======
         model = Funcionario
->>>>>>> ce1a5a5 (test: Adicionando testes aos itens e usuários)
         fields = '__all__'
